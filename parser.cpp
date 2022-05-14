27e3112bc076ac1136a9d70ecf1254d88710a972
Marcus Vinícius <58491006+Marcus-Vn@users.noreply.github.com>
2022-05-13 23:34:22 -0300

    att parser

--- parser.cpp
+++ parser.cpp
@@ -2,10 +2,29 @@
 #include <iostream>
 #include <stdio.h>
 
+Token token;
+void r_id();
+void r_bloco();
+
+void r_programa(){
+  token = proximo_token();
+  if (token.nome_token == PROGRAM ){
+    r_id();
+    token = proximo_token();
+    if(token.nome_token == ';'){
+      r_bloco();
+    } 
+  }
+}
+
+void r_id(){ 
+}
+
+void r_bloco(){ 
+}
+
 int main()
 {
-    Token token;
-
     while (token.nome_token != EOF) {
         token = proximo_token();
         if (token.nome_token == '.' )
